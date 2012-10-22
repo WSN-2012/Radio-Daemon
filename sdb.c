d7cf0f6b6fa81acd1ef75421ac8b057d316677ec
Andreas Torbiörnsson <torbi@kth.se>
2012-10-22 10:01:37 +0200

    Initial commit

---
+++ sdb.c
@@ -0,0 +1,58 @@
+/*NOTE: FUNCTION DOES NOT HANDLE QUERY THAT RETURNS EMPTY RESULT WELL*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+
+#include "sdb.h"
+
+/*Callback function for sqlite3_exec. Stores query result in a timeslot_t*/
+int exec_callback(void* user_param,int columns,char** result,char** names){
+	timeslot_t* t = (timeslot_t *) user_param;
+	t->start_time = (time_t)atoi(result[1]);
+	t->stop_time = (time_t)atoi(result[2]);
+	t->frequency = atoi(result[0]);
+
+	return 0;
+}
+
+/*Function that looks up a timeslot for a given location at the present time and stores the timeslot info in the timeslot_t referenced by its second parameter*/
+int get_timeslot(int location, timeslot_t* tslot){
+	sqlite3* db;
+	int retval;
+	char query[QUERY_LEN];
+
+	/*Insert location into the sql query string*/
+	sprintf(query, SQL_SELECT, location);
+
+	/*Open database*/
+	retval = sqlite3_open(DB_PATH, &db);
+	if(retval){
+		perror("\nCould not connect to db... exiting\n");
+		return 1;
+	}
+
+	/*Execute sql query*/
+	retval = sqlite3_exec(db, query, exec_callback, tslot, NULL);
+	
+	if(retval!=SQLITE_OK){
+		perror("\nSomething went wrong when querying. SQL Error Code: \n");
+		printf("%d", retval);
+		return 1;
+	}
+
+	/*Close database connection*/
+	sqlite3_close(db);
+
+	/*Printing results for debugging purposes*/
+	printf("Got start time: %d",tslot->start_time);
+	printf("\n");
+	printf("Got stop time: %d",tslot->stop_time);
+	printf("\n");
+	printf("Got frequency: %d",tslot->frequency);
+	printf("\n");
+
+	return 0;
+
+}
+
---
+++ sdb.h
@@ -0,0 +1,32 @@
+/* 
+Author: Andreas Torbiörnsson, Technology Transfer Alliance 2012
+Created on 19th October 2012
+*/
+
+#ifndef SDB_H_
+#define SDB_H_
+
+/*Path to the sqlite spectrum database file*/
+#define DB_PATH "./sdb.sqlite3"
+
+/*SQL Query string for getting a timeslot and frequency*/
+#define SQL_SELECT "SELECT frequency, strftime('%%s',start_time), strftime('%%s',stop_time) FROM KistaSDB_2012 WHERE location = %d AND strftime('%%s', 'now') < strftime('%%s', stop_time) ORDER BY start_time ASC LIMIT 1"
+
+/*Max length of the SQL Query string*/
+#define QUERY_LEN 250
+
+/*Format of the timeslot*/
+struct timeslot {
+	time_t start_time;
+	time_t stop_time;
+	int frequency;
+};
+typedef struct timeslot timeslot_t;
+
+/*Function definitions*/
+int exec_callback(void* user_param,int columns,char** result,char** names);
+
+int get_timeslot(int location, timeslot_t* tslot);
+
+
+#endif
